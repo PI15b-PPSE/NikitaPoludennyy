cbb194a7ba0c686c9913b2cc1c60d9bbe57336f2
Marlin <nik.poludennyy@mail.ru>
2018-05-28 18:30:43 +0300

    add self-doc code

--- Js/play_song.js
+++ Js/play_song.js
@@ -1,3 +1,9 @@
+/**
+* Обработка нажатия
+* Первый аргумент - адрес картинки, отображаемой при загрузке страницы
+* Последующие аргументы - адреса воспроизводимых при загрузке страницы звуков
+* Вызов для каждого из звуков функции воспроизведения
+*/
 $(document).ready(function(){
     $('a').click(function(e){
         e.preventDefault();
@@ -15,9 +21,14 @@ $(document).ready(function(){
 		play_sound(path_music4);
 		play_sound(path_music5);
 		play_sound(path_music6);
-		
 	});
 
+/**
+* Функция воспроизведения звука по пути {path_m}
+* Создание объекта snd - класса Audio с передаваемым аргументом path_m
+* Установка режима предзагрузки - auto
+* Воспроизведение звука
+*/
 	function play_sound(path_m){
 	    var snd = new Audio(path_m);
 	    snd.preload = "auto";
